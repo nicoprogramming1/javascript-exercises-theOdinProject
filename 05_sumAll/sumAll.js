0a1b2b0c4575ecbf0e6bb23f55bd26d3064e1df7
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-06-04 19:48:16 -0300

    Fixed function sumAll()
    The function now passes all tests in sumAll.spec.js
    
    Changes to be committed:
    	modified:   05_sumAll/sumAll.js
    	modified:   05_sumAll/sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if(
+        num1 < 0 ||
+        num2 < 0 ||
+        typeof num1 !== 'number' ||
+        typeof num2 !== 'number'
+    )
+        { return "ERROR"; }
+
+
+    let min = Math.min(num1, num2);
+    let max = Math.max(num1, num2);
+    let sumatoria = 0;
+    console.log(sumatoria);
+
+    for(let i = min; i <= max; i++) {
+        sumatoria += i;
+        console.log(sumatoria);
+    }
+    return sumatoria;
 };
 
 // Do not edit below this line
